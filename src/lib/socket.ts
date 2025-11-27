b9883ccad9a2a57d090f5564c256ea2a5620439a
FUN MARKET <funmarketmn@gmail.com>
2025-11-27 02:33:40 +0000

    firebase-studio-1764196890126.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudwo

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,4 +2,6 @@
 import { io } from 'socket.io-client';
 
 // This will only be executed on the client
-export const socket = io('wss://ws.dev.fun/app-4129fba431f3c36d8469');
+export const socket = io('wss://ws.dev.fun/app-4129fba431f3c36d8469', {
+    withCredentials: false
+});
